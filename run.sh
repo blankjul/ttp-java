f5dcd810e516f68f8bc11987ae99c4e87005cc0b
julesy <jules89@arcor.de>
2015-07-24 09:40:04 +0200

    run.sh allows now to define the maxEvaluations!

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 rm -rf experiment
 mkdir experiment
 
+if [ -n "$1" ]
+then  MAXEVAL=$1
+else MAXEVAL=25000
+fi
+
+
+echo "Evaluation Set to "
+echo $MAXEVAL 
+
 mvn clean install
-mvn exec:java -Dexec.mainClass="com.moo.ttp.Study" -Dexec.args="25000"
+mvn exec:java -Dexec.mainClass="com.moo.ttp.Study" -Dexec.args="$MAXEVAL"
 
 Rscript Evaluation.R
